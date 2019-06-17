a6f096a337dc363435060e92fb952d2830f3ccce
medigakoushik <medigasaikoushik@gmail.com>
2019-06-17 19:14:54 +0530

    now randomly colors are generated and filled in the array

--- javascript.js
+++ javascript.js
@@ -1,12 +1,5 @@
 
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedcolor= pickcolor();
@@ -34,7 +27,6 @@ for (var i=0; i<squares.length;i++)
       	}
     });
 
-
 }
 
 function changecolors(color){
@@ -49,4 +41,29 @@ function changecolors(color){
 function pickcolor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	 var arr=[];
+	 //add num random colors to array
+	 for(var i=0; i<num;i++)
+	 {
+	 	//get random color from the function called and push it into the array
+          arr.push(randomColor());
+	 }
+	 return arr;
+}
+
+//function generating random colors
+
+function randomColor(){
+
+	 var r = Math.floor(Math.random() * 256);
+
+	 var g = Math.floor(Math.random() * 256);
+
+	 var b = Math.floor(Math.random() * 256);
+
+	 return "rgb(" + r + "," + g + "," + b + ")";
+
 }
