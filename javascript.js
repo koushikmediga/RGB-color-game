ea8d1e106babe825f6dbfee0130239f9de46a03f
medigakoushik <medigasaikoushik@gmail.com>
2019-06-17 18:41:21 +0530

    till now picked a color and wrote hardcode to it to see if it matches alerted correct else wrong

--- javascript.js
+++ javascript.js
@@ -9,8 +9,25 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
+var pickedcolor=colors[3];
+var colorDisplay=document.getElementById("colordisplay");
+
+
+colorDisplay.textContent=pickedcolor;
 
 for (var i=0; i<squares.length;i++)
 {
-	squares[i].style.background= colors[i];
+	squares[i].style.backgroundColor= colors[i];
+
+	//add click listeners to squares
+    squares[i].addEventListener("click",function(){
+      var clickedcolor=this.style.backgroundColor;
+      if(clickedcolor===pickedcolor){
+      	alert("correct");
+      }
+      	else{
+      		alert("wrong");
+      	}
+    });
+
 }
